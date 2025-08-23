6b883433c0e8f3bab65c7ff08d5c93f57f01640e
Hot6Mania <than7683@gmail.com>
2025-08-24 08:49:13 +0900

    fix db bug

--- alembic/env.py
+++ alembic/env.py
@@ -1,31 +1,44 @@
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
+# alembic/env.py
+from __future__ import annotations
+
 from alembic import context
+from sqlalchemy import create_engine, pool
 
-from app.db.base_class import Base
 from app.core.config import settings
+from app.db.base import target_metadata
 
-config = context.config
-if not config.get_main_option("sqlalchemy.url"):
-    config.set_main_option("sqlalchemy.url", settings.db_url_sync)
 
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
-target_metadata = Base.metadata
-
-def run_migrations_offline():
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True, compare_type=True)
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = settings.db_url_sync
+    print(f"*** ALEMBIC OFFLINE url={url}")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+        compare_server_default=True,
+    )
     with context.begin_transaction():
         context.run_migrations()
 
-def run_migrations_online():
-    connectable = engine_from_config(config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool)
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    url = settings.db_url_sync
+    print(f"*** ALEMBIC ONLINE url={url}")
+    connectable = create_engine(url, poolclass=pool.NullPool)
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            compare_server_default=True,
+        )
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,5 @@
 from app.db.base_class import Base
 from app.models.user import User
 from app.models.identity import Identity
+
+target_metadata = Base.metadata
